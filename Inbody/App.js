86b8ebfc2a07c1034c5145b0426b10064c871477
DESKTOP-U0JP6LC\amzzz <athibet@home.co.th>
2020-12-27 00:03:42 +0700

    First init

---
+++ Inbody/App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+          <View>
+            <Text>init App</Text>
+          </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
